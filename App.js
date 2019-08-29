b1d10fe545f14362cf4e9aff1d8d6135cdef683e
riefin <52729176+arifin26@users.noreply.github.com>
2019-08-29 17:31:47 +0700

    Update App.js

--- App.js
+++ App.js
@@ -6,96 +6,18 @@
  * @flow
  */
 
-import React from 'react'
-import {View,Text,Image,ActivityIndicator,TextInput} from 'react-native'
-import Coba from './test/ulangan'
+import React, {Component} from 'react'
+import {View} from 'react-native'
+import Route from './test/navigasi'
 
-
-export default class ulanagan extends React.Component{
-
-  state={
-    role:true
-}
-
-render(){
-      setTimeout(()=>{
-  this.setState({
-      role:false
-  })
-} ,1000)
-if (this.state.role) {
-  return (
-    <View style={{alignItems:'center',paddingTop:200}}>
-      <Image
-            source={require('./gambar/plant.png')} 
-            style={{height:100,width:100}}
-            
-      />
-      <ActivityIndicator size='large' />
-      <View style={{alignItems:'center'}} >
-            <Text style={{color:'#fff'}}>selamat datang</Text>
-            
+export default class App extends Component{
+  render(){
+    return(
+      <View>
+        <Route/>
       </View>
-    </View>
-  );
-  
-}
-return(
-  <View style={{flex:1}}>
-  <View style={{backgroundColor:'blue',height:50,width:'100%'}}>
-    <View style={{alignItems:'center',paddingTop:10}}>
-    <Coba gambar='PONDOK NEWS'  />
-    <Text >{this.props.gambar}</Text>
-   
-    </View>
-  </View>
-
-    <View style={{paddingTop:10,paddingLeft:20,flexDirection:'row'}}>
-      <Image source={require('./gambar/flower_close_up_leaves_85207_1024x768.jpg')} style={{height:60,width:60,borderRadius:40}}/>
-      <View style={{flexDirection:'column',paddingTop:5}}>
-      <Text style={{paddingLeft:10,fontSize:20}}>AL-Quran dan As-Sunnah</Text>
-      <Text style={{paddingLeft:10,fontSize:12}}>pedoman</Text>
-      </View>
-    </View>
-
-
-
-    <View style={{alignItems:'center',paddingTop:10}}>
-      <Image source={require('./gambar/quran.jpg')} style={{height:150,width:300}}/>
-    </View>
-    <View style={{paddingLeft:20,paddingRight:20}}>
-        <TextInput  placeholder='developer react native' underlineColorAndroid={'black'}/>
-      </View>
-
-
-
-      <View style={{paddingTop:5,paddingLeft:20,flexDirection:'row'}}>
-      <Image source={require('./gambar/Willienlg.jpg')} style={{height:60,width:60,borderRadius:40}}/>
-      <View style={{flexDirection:'column',paddingTop:5}}>
-      <Text style={{paddingLeft:10,fontSize:20}}>Harta Riba</Text>
-      <Text style={{paddingLeft:10,fontSize:12}}>pendefisian</Text>
-      </View>
-    </View>
-
-
-
-
-    <View style={{alignItems:'center',paddingTop:10}}>
-      <Image source={require('./gambar/Emasbatangan.jpg')} style={{height:150,width:300}}/>
-      
-    </View>
-    <View style={{paddingLeft:20,paddingRight:20}}>
-        <TextInput  placeholder='developer react native' underlineColorAndroid={'black'}/>
-      </View>
-
-    
-
-    
-    
-
-  </View>
-)
-}
+    )
+  }
 }
 
 
